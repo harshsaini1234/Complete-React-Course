1300b313b7ee3d19682925ffb8e79a2f12ea0727
Harsh Saini <harshsaini567654@gmail.com>
2026-06-18 12:38:16 +0530

    Today I studied about 3 way to apply css in react

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,23 +1,43 @@
-import React from 'react'
-
-const Product = ({name,price,color,data={}}) => {
-   
+import React from "react";
+import "./Product.css";
+const Product = ({ name, price, color, data = {} }) => {
+  //Internal CSS in React using style attribute
+  const myStyle ={
+    backgroundColor: "yellow",
+    color: "black",
+    padding: "10px",
+    margin: "10px",
+    border: "1px solid blue",
+    borderRadius: "10px",
+  }
   return (
-   <div>
-    <h1>Product - {name}</h1>
-    <h1>price - {price}</h1>
-    <h1>color- {color}</h1>
-    {data.ram && (
-      <>
-        <p>Ram - {data.ram}</p>
-        <p>ROM - {data.rom}</p>
-        <p>Chipset - {data.chipset}</p>
-      </>
-    )   
-    }
-   </div>
-    
-  )
-}
+    // Inline styling in react using style attribute
+    <div
+      // style={{
+      //   backgroundColor: "blue",
+      //   padding: "10px",
+      //   margin: "10px",
+      //   border: "1px solid yellow",
+      //   borderRadius: "10px",
+      // }}
+      //Internal CSS in React using style attribute
+      // style={myStyle}
+     // External CSS in React using className attribute
+     className="con"
+      // className="container"
+    >
+      <h1>Product - {name}</h1>
+      <h1>price - {price}</h1>
+      <h1>color- {color}</h1>
+      {data.ram && (
+        <>
+          <p>Ram - {data.ram}</p>
+          <p>ROM - {data.rom}</p>
+          <p>Chipset - {data.chipset}</p>
+        </>
+      )}
+    </div>
+  );
+};
 
-export default Product
+export default Product;
